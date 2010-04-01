7777cd434936f9f5c77697b0d1a60db899a9da88
Nilesh Chaudhari <mail@nilesh.org>
2010-04-01 16:25:30 +0530

    Added few more methods

--- lib/rchart.rb
+++ lib/rchart.rb
@@ -1,6 +1,7 @@
 require 'ruby-gd'
 
 class RChart
+  
   SCALE_NORMAL = 1
   SCALE_ADDALL = 2
   SCALE_START0 = 3
@@ -22,9 +23,38 @@ class RChart
   ALIGN_BOTTOM_RIGHT = 9
   
   def initialize(options={})
+    
+    # Initialize variables
     raise ArgumentError if (options[:x_size].nil? && options[:y_size].nil?)
     @x_size = options[:x_size]
     @y_size = options[:y_size]
+    @error_reporting = false
+    @error_font_name = "../fonts/pf_arma_five.ttf"
+    @error_font_size = 6
+    @currency = "Rs."
+    @date_format = "%d/%m/%Y"
+    @line_width = 1
+    @line_dot_size = 0
+    @anti_alias_quality = 0
+    @shadow_active = false
+    @shadow_x_distance = 1
+    @shadow_y_distance = 1
+    @shadow_r_color = 60
+    @shadow_g_color = 60
+    @shadow_b_color = 60
+    @shadow_alpha = 50
+    @shadow_blur = 0
+    @tmp_dir = '/tmp'
+    
+    # $this->Picture = imagecreatetruecolor($XSize,$YSize);
+    # 
+    #  $C_White =$this->AllocateColor($this->Picture,255,255,255);
+    #  imagefilledrectangle($this->Picture,0,0,$XSize,$YSize,$C_White);
+    #  imagecolortransparent($this->Picture,$C_White);
+    # 
+    #  $this->setFontProperties("tahoma.ttf",8);
+    
+    
   end
   
   def draw_background(r, g, b)
